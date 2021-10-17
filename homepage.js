e20337b9d6d3d830270197fd6ff69edcf021edb2
jjia131 <92062758+jjia131@users.noreply.github.com>
2021-10-17 11:30:24 -0400

    Update homepage.js
    Update

--- homepage.js
+++ homepage.js
@@ -58,19 +58,45 @@ function get_recipe() {
         if (typeof recipe_instructions_3 === 'undefined') 
             recipe_instructions_3 = results[2].recipes;
 
+        let data_s = recipe_instructions_1;
+        let list = document.getElementById("recipe_instructions_1");
+  
+        data_s.forEach((item) => {
+            let li = document.createElement("li");
+            li.innerText = item.description;
+            list.appendChild(li);
+        });
+
+         data_s = recipe_instructions_1;
+         list = document.getElementById("recipe_instructions_2");
+  
+        data_s.forEach((item) => {
+            let li = document.createElement("li");
+            li.innerText = item.description;
+            list.appendChild(li);
+        });
+
+        data_s = recipe_instructions_1;
+        list = document.getElementById("recipe_instructions_3");
+  
+        data_s.forEach((item) => {
+            let li = document.createElement("li");
+            li.innerText = item.description;
+            console.log(item);
+            list.appendChild(li);
+        });
+        
+
+
             document.getElementById("recipe_name_1").innerHTML = results[0].name;
             document.getElementById("recipe_name_2").innerHTML = results[1].name;
             document.getElementById("recipe_name_3").innerHTML = results[2].name;
-            document.getElementById("recipe_instructions_1") = recipe_instructions_1;
-            document.getElementById("recipe_instructions_2") = recipe_instructions_2;
-            document.getElementById("recipe_instructions_3") = recipe_instructions_3;
-            document.getElementById("recipe_url_1") = results[0].thumbnail_url;
-            document.getElementById("recipe_url_2") = results[1].thumbnail_url;
-            document.getElementById("recipe_url_3") = results[2].thumbnail_url;
-            
-            console.log(recipe_name);
-            console.log(recipe_instructions);
-            console.log(recipe_url);
+            //document.getElementById("recipe_instructions_1") = recipe_instructions_1;
+            //document.getElementById("recipe_instructions_2") = recipe_instructions_2;
+            //document.getElementById("recipe_instructions_3") = recipe_instructions_3;
+            //document.getElementById("recipe_url_1") = results[0].thumbnail_url;
+            //document.getElementById("recipe_url_2") = results[1].thumbnail_url;
+            //document.getElementById("recipe_url_3") = results[2].thumbnail_url;
         
     })
     .catch(err => {
