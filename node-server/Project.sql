e176cee52839f1395f7ceac2a23dc552c347505b
Giyuu <tranhoanglong1904@gmail.com>
2021-10-16 22:44:15 -0400

    create node-server

---
+++ node-server/Project.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE HackRU;
+
+USE HackRU;
+
+CREATE TABLE recipeList(
+	recipe_id INT NOT NULL AUTO_INCREMENT,
+	recipe_name varchar(255),
+    tags varchar(255),
+    image varchar(255),
+    descrip varchar(255),
+    primary key(recipe_id)
+);
+
+CREATE TABLE instruction(
+	recipe_id INT NOT NULL,
+    instruction_id INT NOT NULL AUTO_INCREMENT,
+    texts varchar(255),
+    PRIMARY KEY (instruction_id)
+);
