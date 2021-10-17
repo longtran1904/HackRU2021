eac76e57cf3b4f9eb939955867ec1c3fe33bf679
Giyuu <tranhoanglong1904@gmail.com>
2021-10-17 11:13:27 -0400

    get instructions

--- homepage.js
+++ homepage.js
@@ -48,19 +48,26 @@ function get_recipe() {
     .then(data => {
         var results = data.results;
         console.log(results);
-    
+        recipe_instructions_1 = results[0].instructions;
+        if (typeof recipe_instructions_1 === 'undefined') 
+            recipe_instructions_1 = results[0].recipes;
+        recipe_instructions_2 = results[1].instructions;
+        if (typeof recipe_instructions_2 === 'undefined') 
+            recipe_instructions_2 = results[1].recipes;
+        recipe_instructions_3 = results[2].instructions;
+        if (typeof recipe_instructions_3 === 'undefined') 
+            recipe_instructions_3 = results[2].recipes;
+
             document.getElementById("recipe_name_1").innerHTML = results[0].name;
             document.getElementById("recipe_name_2").innerHTML = results[1].name;
             document.getElementById("recipe_name_3").innerHTML = results[2].name;
-            document.getElementById("recipe_instructions_1") = results[0].recipes;
-            document.getElementById("recipe_instructions_2") = results[1].recipes;
-            document.getElementById("recipe_instructions_3") = results[2].recipes;
+            document.getElementById("recipe_instructions_1") = recipe_instructions_1;
+            document.getElementById("recipe_instructions_2") = recipe_instructions_2;
+            document.getElementById("recipe_instructions_3") = recipe_instructions_3;
             document.getElementById("recipe_url_1") = results[0].thumbnail_url;
             document.getElementById("recipe_url_2") = results[1].thumbnail_url;
             document.getElementById("recipe_url_3") = results[2].thumbnail_url;
-            if (typeof recipe_instructions === 'undefined') 
             
-            recipe_instructions = results[i].recipes;
             console.log(recipe_name);
             console.log(recipe_instructions);
             console.log(recipe_url);
