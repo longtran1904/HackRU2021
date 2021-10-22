6744fb989f11cf13f324fc61fceb3ae253ab7eba
jjia131 <92062758+jjia131@users.noreply.github.com>
2021-10-22 11:55:19 -0400

    Update homepage.js
    Get url for image

--- homepage.js
+++ homepage.js
@@ -1,15 +1,18 @@
 var recipe_name_1;
 var recipe_name_1;
 var recipe_name_1;
+
 var recipe_instructions_1;
-
 var recipe_instructions_2;
-
 var recipe_instructions_3;
 
 var recipe_url_1;
 var recipe_url_2;
 var recipe_url_3;
+
+var imagename_1;
+var imagename_2;
+var imagename_3;
 
 console.log("hello world");
 
@@ -47,23 +50,25 @@ function get_recipe() {
         response.json())
     .then(data => {
         var results = data.results;
-        console.log(results);
-        recipe_instructions_1 = results[2].instructions;
-        if (typeof recipe_instructions_1 === 'undefined') 
-            recipe_instructions_1 = results[2].recipes;
-        recipe_instructions_2 = results[2].instructions;
-        if (typeof recipe_instructions_2 === 'undefined') 
-            recipe_instructions_2 = results[2].recipes;
+        
+        recipe_instructions_1 = results[0].instructions;
+        //if (typeof recipe_instructions_1 === 'undefined') 
+       //     recipe_instructions_1 = results[2].recipes;
+        recipe_instructions_2 = results[1].instructions;
+        //if (typeof recipe_instructions_2 === 'undefined') 
+        //    recipe_instructions_2 = results[2].recipes;
         recipe_instructions_3 = results[2].instructions;
-        if (typeof recipe_instructions_3 === 'undefined')
-            recipe_instructions_3 = results[2].recipes;
+        //if (typeof recipe_instructions_3 === 'undefined')
+       //     recipe_instructions_3 = results[2].recipes;
+
+
 
         let data_f = recipe_instructions_1;
         let list = document.getElementById("recipe_instructions_1");
   
         data_f.forEach((item) => {
             let li = document.createElement("li");
-            li.innerText = item.display_text;
+            li.innerText = item.display_text.trim();
             list.appendChild(li);
         });
 
@@ -72,7 +77,7 @@ function get_recipe() {
   
         data_s.forEach((item) => {
             let li = document.createElement("li");
-            li.innerText = item.display_text;
+            li.innerText = item.display_text.trim();
             list_s.appendChild(li);
         });
 
@@ -81,23 +86,24 @@ function get_recipe() {
   
         data_t.forEach((item) => {
             let li = document.createElement("li");
-            li.innerText = item.display_text;
+            li.innerText = item.display_text.trim();
             list_t.appendChild(li);
         });
         
 
-            document.getElementById("recipe_name_1").innerHTML = results[0].name;
-            document.getElementById("recipe_name_2").innerHTML = results[1].name;
-            document.getElementById("recipe_name_3").innerHTML = results[2].name;
-            //document.getElementById("recipe_instructions_1") = recipe_instructions_1;
-            //document.getElementById("recipe_instructions_2") = recipe_instructions_2;
-            //document.getElementById("recipe_instructions_3") = recipe_instructions_3;
-            //document.getElementById("recipe_url_1") = results[0].thumbnail_url;
-            //document.getElementById("recipe_url_2") = results[1].thumbnail_url;
-            //document.getElementById("recipe_url_3") = results[2].thumbnail_url;
+        document.getElementById("recipe_name_1").innerHTML = results[0].name;
+        document.getElementById("recipe_name_2").innerHTML = results[1].name;
+        document.getElementById("recipe_name_3").innerHTML = results[2].name;
+
+
+        document.getElementById('imagename_1').src = results[0].thumbnail_url;
+        document.getElementById('imagename_2').src = results[1].thumbnail_url;
+        document.getElementById('imagename_3').src = results[2].thumbnail_url;
         
     })
     .catch(err => {
         console.error(err);
     });
+
+    
 }
